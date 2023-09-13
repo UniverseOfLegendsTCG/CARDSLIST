b8fbc6be2d8675eee341838680851bb53d429535
UniverseOfLegendsTCG <144827527+UniverseOfLegendsTCG@users.noreply.github.com>
2023-09-12 20:26:58 -0400

    update test

--- card.js
+++ card.js
@@ -8,5 +8,15 @@ export const cards = [
     subtype: "Magic",
     effect: `Arrive: Summon a 3 or less cost minion from the unknown.  3 Cost: Summon a 4 or less cost Legend from the universe.  Send it to the unknown at the end of the turn.`,
     number: "001",
-  }
+  },
+  {
+    image: "Marie.png",
+    name: "Marie, Misunderstood Witch",
+    color: "purple",
+    cost: "5",
+    type: "Legend",
+    subtype: "Magic",
+    effect: `Arrive: Summon a 3 or less cost minion from the unknown.  3 Cost: Summon a 4 or less cost Legend from the universe.  Send it to the unknown at the end of the turn.`,
+    number: "002",
+  },
 ];
